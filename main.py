9bf1ac50f540f202c29fb30191a1f7b0e77a6e45
Sh1ynx <sh1ynx@proton.me>
2026-05-25 15:04:03 +0300

    splitting the packet header and identifying the packet protocol

--- main.py
+++ main.py
@@ -11,11 +11,36 @@ options = parser.parse_args()
 
 
 class Packet:
-    pass
+    def __init__(self, packet):
+        self.packet = packet
+        header = struct.unpack('<BBHHHBBH4s4s', packet[0:20])
+        self.version = header[0] >> 4
+        self.header_length = header[0] & 0xF
+        self.type_of_service = header[1]
+        self.bit_total_length = header[2]
+        self.bit_identification = header[3]
+        self.flag_and_fragment_offset = header[4]
+        self.time_to_live = header[5]
+        self.bit_protocol = header[6]
+        self.checksum = header[7]
+        self.source_ip = header[8]
+        self.dest_ip = header[9]
+
+        self.protocols = {
+            1: "ICMP",
+            6: "TCP",
+            17:"UDP"
+        }
+
+        try:
+            self.protocol = self.protocols[self.bit_protocol]
+        except Exception as e:
+            print(f"{e} Protocol doesn't exist or not supported")
+            self.protocol = str(self.bit_protocol)
+
 
 def sniff():
     pass
-
 
 if __name__ == "__main__":
     sniff()
